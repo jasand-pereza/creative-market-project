ac51d0c5cce03b78b45100ed3ff10194e496b8f7
Jasand <jasand@crowandraven.co>
2018-08-27 13:55:01 -0600

    backend form validation

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Auth;
 use Illuminate\Http\Request;
+use App\User;
 
 class UsersController extends Controller
 {
@@ -17,8 +18,57 @@ class UsersController extends Controller
     
     }
 
-    public function applyForTrial(Request $request)
+    /**
+     * Is the email valid
+     * @param  String  $email
+     * @return Boolean
+     */
+    public static function isEmailValid($email) 
     {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return false;
+        }
+        list($user, $domain) = explode('@', $email);
+        if (!checkdnsrr($domain, 'MX')) {
+            return false;
+        }
 
+        return true;
+    }
+
+    /**
+     * Does an email exist in the users table
+     * @param  Request  $request
+     * @return json
+     */
+    public function userEmailExists(Request $request)
+    {
+        if (!$request->input('email')) {
+            return response()->json([
+                'status' => 'fail',
+                'response_code' => 1,
+                'message' => 'Not enough arguments. No email supplied.',
+            ]);
+        }
+        if (!self::isEmailValid($request->input('email'))) {
+            return response()->json([
+                'status' => 'fail',
+                'response_code' => 4,
+                'message' => 'The email specified is invalid.',
+            ]);
+        }
+        if (User::where('email', $request->input('email'))->first()) {
+            return response()->json([
+                'status' => 'success',
+                'response_code' => 2,
+                'message' => 'User exists in system.',
+            ]);
+        } else {
+            return response()->json([
+                'status' => 'success',
+                'response_code' => 3,
+                'message' => 'User with email specified does not exist.',
+            ]);
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('signup');
 });
+
+Route::post('/user-exists', 'UsersController@userEmailExists');
